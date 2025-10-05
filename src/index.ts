477e0ac325979d8987f6363138d471b0daf54654
Agustin Tamagno <agusto.tamagno@gmail.com>
2025-10-05 17:25:01 +0700

    Added basic express app with a single route and a single unit test.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import express from "express";
+import dotenv from "dotenv";
+import healthRoutes from "./routes/health";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Routes
+app.use("/api/health", healthRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
