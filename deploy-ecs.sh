e7656badcba7a224326b95fe6f3462152d55bd21
Agustin Tamagno <agusto.tamagno@gmail.com>
2025-10-08 16:18:26 +0700

    Added cfn template to deploy ecs resources containing roles and cluster.

---
+++ deploy-ecs.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+START_TIME=$(date -R)
+GLOBAL_OVERRIDES=$1
+
+set -e  # Exit on any error
+
+export PERMISSIONS_BOUNDARY="none"
+######## Get script parameters separated by ; and set them as global variables #########
+OLD_IFS=$IFS # backup original separator (new line usually) so we can revert it as other code might rely on it
+export IFS=";"
+ALLOWED_KEYS=("AWS_REGION" "ACCOUNT_ID" "ENVIRONMENT_NAME" "IMAGE_TAG")
+for keyVal in $GLOBAL_OVERRIDES; do
+  KEY=${keyVal%=*}
+  VALUE=${keyVal#*=}
+  # Check if KEY is in the whitelist
+  for allowed in "${ALLOWED_KEYS[@]}"; do
+    if [[ "$KEY" == "$allowed" ]]; then
+      export ${KEY}="${VALUE}"
+      break
+    fi
+  done
+done
+export IFS=$OLD_IFS # put separator back to normal
+
+if [ -z "${AWS_REGION}" ]
+then
+  echo "AWS_REGION environment variable is not set. Aborting."
+  exit 1
+fi
+
+if [ -z "${ACCOUNT_ID}" ]
+then
+  export ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+fi
+
+export GIT_HASH=`git rev-parse --short HEAD`
+if [ -z "${GIT_HASH}" ]; then
+  export GIT_HASH="unknown"
+  echo "Using default git hash of unknown"
+fi
+
+export GIT_BRANCH=`git symbolic-ref --short HEAD`
+if [ -z "${GIT_BRANCH}" ]; then
+  export GIT_BRANCH="hash"
+  echo "Using default git branch of hash"
+fi
+
+# Configuration
+APP_NAME="manual-app"
+ENVIRONMENT_NAME="${ENVIRONMENT_NAME:-dev}"
+PREFIX="${APP_NAME}-${ENVIRONMENT_NAME}-"
+POSTFIX="-${ACCOUNT_ID}-${AWS_REGION}"
+
+ECS_CFN_TEMPLATE="cfn/ecs.yml"
+ECS_STACK="${PREFIX}ecs-stack"
+CFN_TAGS="Application=${APP_NAME} Environment=${ENVIRONMENT_NAME}"
+
+aws cloudformation deploy \
+  --stack-name $ECS_STACK \
+  --template-file $ECS_CFN_TEMPLATE \
+  --parameter-overrides \
+      pAppName=$APP_NAME \
+      pEnvironmentName=$ENVIRONMENT_NAME \
+      pGitBranch=$GIT_BRANCH \
+      pGitHash=$GIT_HASH \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --no-fail-on-empty-changeset \
+  --tags $CFN_TAGS \
+  --region $AWS_REGION
+
+END_TIME=$(date -R)
+
+echo "Start time : ${START_TIME}"
+echo "End time   : ${END_TIME}"
